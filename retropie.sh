82fb2104582f28b6ec60238f809c04f478ceb253
Paul Thompson <thompson.paul.carl@gmail.com>
2021-08-07 13:43:13 -0500

    Install RetroPie from command line

--- retropie.sh
+++ retropie.sh
@@ -16,6 +16,7 @@
     git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
     cd RetroPie-Setup
     chmod +x retropie_setup.sh
-    sudo ./retropie_setup.sh
+    chmod +x retropie_packages.sh
+    sudo ./retropie_packages.sh setup basic_install
 
     
